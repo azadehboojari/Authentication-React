8289b7c1fe97c7ed65ab5e4694f8134f519e1cf5
azadehboojari <39608077+azadehboojari@users.noreply.github.com>
2019-12-01 19:28:01 -0800

    Update GoogleAuth.js

--- components/GoogleAuth.js
+++ components/GoogleAuth.js
@@ -3,14 +3,10 @@ import React from 'react';
 class GoogleAuth extends React.Component {
     state={ isSignedIn: null }
     componentDidMount(){
-        // we use window to sya this is a variable GAPI that is 
-        // available on windows scope inside our browser
-        // without window browser will throw an error that GAPI is unknown
-        // because loading this library will take som etime we need to give the function
-        // a callback of when that process is complete
+    
         window.gapi.load('client:auth2', ()=>{
             window.gapi.client.init({
-                clientId: '484436129610-3pt8f02e3rqqvgtdu40gdbjtvds378lr.apps.googleusercontent.com',
+                clientId: 'deleted',
                 scope: 'email'
             }).then(()=>{
                 this.auth=window.gapi.auth2.getAuthInstance();
